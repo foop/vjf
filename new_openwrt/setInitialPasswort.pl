360844d982eb11004c883394ac23aca47d03a847
Dominik Danter <dominik@foop.at>
2012-12-05 18:59:20 +0100

    setInitialPassword can now read password from file

--- new_openwrt/setInitialPasswort.pl
+++ new_openwrt/setInitialPasswort.pl
@@ -3,14 +3,30 @@ use Net::Telnet ();
 use strict;
 use 5.010;
 use warnings;
-
+my $passwd_file = "./default_passwd";
+my $fallback_passwd = "admin";
 my $ip_addr = shift(@ARGV) || "192.168.1.1";
-my $passwd = shift(@ARGV) || "default_password";
+
+#set password
+my $passwd = shift(@ARGV);
+unless ( defined $passwd ) {
+    if ( -r $passwd_file ) {
+        open (my $fh,  '<', $passwd_file) or die "Weird, could not read $passwd_file";
+        ($passwd) = <$fh> =~ /password=([\S]+)/;
+        close $fh;
+        unless (defined $passwd) die "Although you provided the file $passwd_file, I could not parse it. 
+              You're password file's first line should be:
+              password=yourpassword
+              *No whitespace allowed!*";
+    }
+}
+$passwd = $fallback_passwd unless defined $passwd;
+say $passwd;
+
 my $prompt_default = 'm&root\@OpenWrt:/#\s&' ;
 my $prompt_password = "/New password:/";
 my $prompt_password_2 = "/Retype password:/";
 my $t;
-
 $t = new Net::Telnet (Timeout => 10);
 $t->open($ip_addr);
 $t->waitfor($prompt_default);
